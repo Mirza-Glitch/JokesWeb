d6208f6615f13364d7a53d88d391731dce004d06
Mirza-Glitch <89267349+Mirza-Glitch@users.noreply.github.com>
2023-03-01 00:03:53 +0530

    Create Empty.jsx

---
+++ src/components/Empty.jsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Empty() {
+
+  return (
+
+    <div className="empty">
+
+      <div className="container">
+
+        <h3 id="joke"> You have saved no Jokes ☹️ </h3>
+
+      </div>
+
+    </div>
+
+  );
+
+}
+
+export default Empty;
+
